7d679673376c477800248906b0b1c97a1c43cf05
Minh Le <184964779+lebm646@users.noreply.github.com>
2025-12-09 18:05:26 -0800

    Add project files.

---
+++ GPSHeatMap/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GPSHeatMap
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GPSHeatMap/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace GPSHeatMap
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
